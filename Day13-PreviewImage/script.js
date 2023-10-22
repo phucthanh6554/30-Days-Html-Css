8dabe7f92daf92b77c67aacb31b2d6f62b909b5d
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2023-10-22 09:46:21 +0700

    Day 13: Preview image

---
+++ Day13-PreviewImage/script.js
@@ -0,0 +1,11 @@
+const uploadInput = document.getElementById('upload-img')
+const previewImage = document.getElementById('preview-img')
+
+uploadInput.onchange = function(e) {
+    let file = e.target.files[0]
+
+    if(file != null) {
+        previewImage.setAttribute('src', URL.createObjectURL(file))
+    }
+}
+
